311b6dbfdc86c24099663e89d28031957995335a
MesKieran <84025183+MesKieran@users.noreply.github.com>
2021-11-22 14:27:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from flask import abort
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRETE_KEY")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 # app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap(app)
